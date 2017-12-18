234cd7d012cd2185a2bc6f632532f98a0c513b9d
mtharpie <tharp.matthew@gmail.com>
2017-12-17 22:07:46 -0600

    Tenant 1 final

--- vagrant/scripts/ztp.sh
+++ vagrant/scripts/ztp.sh
@@ -5,6 +5,10 @@ FastCli -p 15 -c"
 enable
 configure
 
+username ansible-user privilege 15 secret 0 arista123 role network-admin
+
+aaa authorization exec default local
+
 management api http-commands
    protocol http
    no shutdown
